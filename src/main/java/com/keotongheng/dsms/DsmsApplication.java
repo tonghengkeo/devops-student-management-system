04ce7bc64772d7922b3b4225539ea60547fcaf1d
Keo Tongheng <keotonghengkeo@gmail.com>
2026-09-25 02:00:43 +0700

    feat: initialize Spring Boot application foundation

---
+++ src/main/java/com/keotongheng/dsms/DsmsApplication.java
@@ -0,0 +1,13 @@
+package com.keotongheng.dsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keotongheng/dsms/DsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keotongheng.dsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
